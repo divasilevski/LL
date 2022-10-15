4830808a4aa97c3a5d8217877cd5b3c6bb0920f1
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2022-10-16 01:18:51 +0300

    fix api fetch hook

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -1,7 +1,10 @@
+import { generateUniqueKey } from '@/utils/common'
+
 type Request<T> = Parameters<typeof useFetch<T>>[0]
 type Options<T> = Parameters<typeof useFetch<T>>[1]
 
 export default function <T>(request: Request<T>, options?: Options<T>) {
+  const key = generateUniqueKey()
   const runtimeConfig = useRuntimeConfig()
   const { baseURL } = runtimeConfig.public
 
@@ -12,5 +15,5 @@ export default function <T>(request: Request<T>, options?: Options<T>) {
     headers.authorization = userToken.value
   }
 
-  return useFetch<T>(request, { baseURL, headers, ...options })
+  return useFetch<T>(request, { key, baseURL, headers, ...options })
 }
