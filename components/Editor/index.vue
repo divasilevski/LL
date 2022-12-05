88f4b9543e8e2b61ff1897eb71bcbd98c4bbe5cd
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2022-12-05 22:22:57 +0300

    add editor js

---
+++ components/Editor/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+const imports = [import('@editorjs/editorjs'), import('@editorjs/header')]
+
+onBeforeMount(async () => {
+  const dynamicImports = await Promise.all(imports)
+  const [EditorJS, Header] = dynamicImports.map((lib) => lib.default)
+
+  const editor = new EditorJS({
+    holder: 'editorjs',
+    // autofocus: true,
+    placeholder: 'Placeholder',
+    tools: {
+      header: {
+        class: Header,
+      },
+    },
+  })
+})
+</script>
+
+<template>
+  <div id="editorjs"></div>
+</template>
+
+<style lang="postcss" scoped>
+#editorjs {
+  width: 100%;
+}
+</style>
---
+++ pages/create.vue
@@ -0,0 +1,5 @@
+<template>
+  <article class="w-full">
+    <Editor />
+  </article>
+</template>
