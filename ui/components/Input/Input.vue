6096f1465149bd8c183c52741a7a640e4a4a9752
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2023-01-01 23:03:57 +0300

    add label to input

--- ui/components/Input/Input.vue
+++ ui/components/Input/Input.vue
@@ -1,10 +1,23 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+})
+</script>
 
 <template>
   <div class="input">
+    <label v-if="props.label" :for="name">{{ props.label }}</label>
     <input
       class="input__textfield"
       v-bind="$attrs"
+      :id="name"
       @input="$emit('update:value', $event.target.value)"
     />
   </div>
@@ -14,9 +27,14 @@
 .input {
   @apply relative;
 
+  label {
+    @apply text-xs;
+  }
+
   .input__textfield {
     @apply flex items-center h-10 px-4 bg-white drop-shadow-md;
     @apply focus:outline-none;
   }
+
 }
 </style>
